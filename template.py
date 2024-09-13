832af563fd7d7f70e9460933ea67b48ebdac671b
Ashokbtech87 <93673077+Ashokbtech87@users.noreply.github.com>
2024-09-13 23:34:41 +0530

    folder stucture updated

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+import logging
+from pathlib import Path
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",  # Added missing comma here
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+for file in list_of_files:
+    filepath = Path(file)  # Corrected variable use
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir:  # Check if filedir is not empty
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    # Check if file does not exist or is empty, and then create an empty file
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
